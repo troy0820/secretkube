1b0bd09f03180ac9a8455868467931e9e51aa878
Troy Connor <troy0820@gmail.com>
2019-04-09 22:55:04 -0400

    🔬[test]Add test to output_test
    Test checks the equality of the secret with what gets passed to the CreateSecret function

--- cmd/output_test.go
+++ cmd/output_test.go
@@ -1,17 +1,32 @@
 package cmd
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestOutputSecret(t *testing.T) {
-	m, err := makeMapfromJson("../json.json")
+	m, err := MakeMapFromJSON("../json.json")
 	if err != nil {
 		t.Error("Error with makeMapfromJson")
 	}
 	t.Run("Created Secrets are equal", func(t *testing.T) {
-
+		bytemap := TurnMapToBytes(m)
+		secret, err := CreateSecret("fancy-secret", bytemap)
+		if err != nil {
+			t.Error("Error with creating secret")
+		}
+		if want, got := "fancy-secret", secret.Name; want != got {
+			t.Errorf("Secret Name is %v and you got %v", want, got)
+		}
+		if want, got := "v1", secret.APIVersion; want != got {
+			t.Errorf("Secret APIVersion is %v and you got %v", want, got)
+		}
+		if want, got := bytemap, secret.Data; reflect.DeepEqual(want, got) != true {
+			t.Errorf("Secret Data is %v and you got %v", want, got)
+		}
 	})
 	t.Run("Output secret equals stdOut", func(T *testing.T) {
 
 	})
-	print(m)
 }
