95f45a0d3fac72024a901d14d1b2eb9365f03dad
Troy Connor <troy0820@gmail.com>
2019-04-29 13:39:24 -0400

    🔧[test]Deleted bytes.Buffer for Version test

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,14 +1,12 @@
 package cmd
 
 import (
-	"bytes"
 	"testing"
 )
 
 func TestVersionCommand(t *testing.T) {
 	vers := "SecretKube -- version 1.0.0"
-	buffer := bytes.NewBufferString(vers)
-	if want, got := buffer.String(), Version("1.0.0"); want != got {
+	if want, got := vers, Version("1.0.0"); want != got {
 		t.Errorf("You wanted %v but got %v", want, got)
 	}
 }
