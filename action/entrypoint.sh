6abd50a8133ad1e5b376e221510fbf51ed16f501
Troy Connor <troy0820@gmail.com>
2019-09-01 02:39:31 -0400

    📦[vendor]Add to vendor directory, remove -mod from build

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -6,6 +6,6 @@ go get ./...
 
 go test ./... -v
 
-go build -mod=vendor -ldflags "-X github.com/troy0820/secretkube/version.Version=v0.0.1" -o secretkube
+go build -ldflags "-X github.com/troy0820/secretkube/version.Version=v0.0.1" -o secretkube
 
 ./secretkube output -f "testdata/json.json" -o output.yaml -n troy -s troyboy
