00aa197c2f357604b019033e6762be350b56e6e3
Troy Connor <troy0820@gmail.com>
2019-11-23 16:54:36 -0500

    [deps]Change back to go get

--- action/build/entrypoint.sh
+++ action/build/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Build the application and test output function
 
-GOOS=linux CGO_ENABLED=0 GOARCH=amd64
-go mod download
+rm go.sum
+
+go get ./...
+
 go build -ldflags "-X github.com/troy0820/secretkube/version.Version=v0.0.1" -o secretkube
 
 ./secretkube output -f "testdata/json.json" -o output.yaml -n secret -s secret
--- action/test/entrypoint.sh
+++ action/test/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Test the Golang application
-GOOS=linux CGO_ENABLED=0 GOARCH=amd64
-go mod download
+rm go.sum
+go get ./...
 go test ./... -v
 
